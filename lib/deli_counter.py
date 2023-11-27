94473367b32877c7512a4305353d07c674d03c33
Davis <daviesnyakiba@gmail.com>
2023-11-27 08:07:56 +0300

    Completed Assignment

---
+++ lib/deli_counter.py
@@ -0,0 +1,12 @@
+def 
+
+def take_a_number(katz_deli, name):
+    katz_deli.append(name)
+    return f"Welcome, {name}. You are number {len(katz_deli)} in line."
+
+def now_serving(katz_deli):
+    if not katz_deli:
+        print("There is nobody waiting to be served!")
+    else:
+        serving = katz_deli.pop(0)
+        print(f"Currently serving {serving}.")
